3cda176fc2ec3ecb6b89a40ef07bedca21a4f782
rajkanwarsingh <rajkanwarsingh@gmail.com>
2023-11-29 18:52:55 +0530

    Formatting changes

--- Project2-Progress-Steps/script.js
+++ Project2-Progress-Steps/script.js
@@ -12,7 +12,6 @@ buttons.forEach((button) => {
       
         if(button.classList.contains('next')) {
             currentActive++
-            console.log('first-check '+ currentActive)
             if (currentActive > steps.length) {
                 currentActive--
             }
